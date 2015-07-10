f2de458f7ff3c044436ca02ea0508afe32134be8
schyzeno <namphan00@gmail.com>
2015-07-10 02:20:43 -0500

    set interval on server side

--- seva-meteor/seva-meteor.js
+++ seva-meteor/seva-meteor.js
@@ -1,119 +1,121 @@
 // seva-meteor.js
 Tasks = new Mongo.Collection("tasks");
+Counters = new Mongo.Collection("counters");
 TriviaCount = new Mongo.Collection("trivaCount");
-PersonalTrivia = new Mongo.Collection("personalTrivia");
 if (Meteor.isClient) {
 	Meteor.subscribe("tasks");
 	Session.setDefault('random_trivia', "Unable to retrieve trivia");
 	Session.setDefault('trivia_count',0);
 	Session.setDefault('counter', 0);
-	setInterval(function(){ var z = Meteor.call("getTriviaCount",function(err, data){
-				if(err){
-					console.log(err);
-					}
-				else
-				{
-					console.log(data);	
-					Session.set('trivia_count',data);
-				}
-			}); }, 60000);
-	// This code only runs on the client
-	Template.body.helpers({
-		tasks: function () {
-			return Tasks.find({});
-		}
-	});
-	
-	Template.trivia.helpers({
-		random_trivia: function () {
-			return Session.get('random_trivia');			
-		},
-		counter: function () {
-			return Session.get('counter');			
-		}
-	});
-	
-	Template.trivia.events({
-		"click .get_trivia": function () {
-			Session.set('counter',Session.get('counter')+1);
-			var x = Meteor.call("getTrivia", function(err, data){
-				if(err){
-					console.log(err);
-					}
-				else
-				{
-					console.log(data);	
-					Session.set('random_trivia',"\""+data+" \"");
-				}
-			});
-			
-			
-		}
-	});
-	
-	Template.body.events({
-		"submit .new-task": function (event) {
-			// This function is called when the new task form is submitted
-			
-			var text = event.target.text.value;
-			
-			Meteor.call("addTask", text);
-			
-		// Clear form
-		event.target.text.value = "";
-		
-		// Prevent default form submit
-		return false;
-		},
-		"change .hide-completed input": function (event) {
-			Session.set("hideCompleted", event.target.checked);
-		}		
-	});
-	
-	// Replace the existing Template.body.helpers
-	Template.body.helpers({
-		tasks: function () {
-			if (Session.get("hideCompleted")) {
-				// If hide completed is checked, filter tasks
-				return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-				} else {
-				// Otherwise, return all of the tasks
-				return Tasks.find({}, {sort: {createdAt: -1}});
+	setInterval(function(){ 
+		console.log("Hello World");
+		var z = Meteor.call("getTriviaCount",function(err, data){
+			if(err){
+				console.log(err);
 			}
-		},
-		hideCompleted: function () {
-			return Session.get("hideCompleted");
-		},
-		incompleteCount: function () {
-			return Tasks.find({checked: {$ne: true}}).count();
-		},
-		trivia_count: function() {
-			return Session.get('trivia_count');
-		}
-	});
-	
-	// In the client code, below everything else
-	Template.task.events({
-		"click .toggle-checked": function () {
-			// Set the checked property to the opposite of its current value
-			Meteor.call("setChecked", this._id, ! this.checked);
-		},
-		"click .delete": function () {
-			Meteor.call("deleteTask", this._id);
-		},
-		"click .toggle-private": function () {
-			Meteor.call("setPrivate", this._id, ! this.private);
-		}
-	});
-	Accounts.ui.config({
-		passwordSignupFields: "USERNAME_ONLY"
-	});
-	Template.task.helpers({
-		isOwner: function () {
-			return this.owner === Meteor.userId();
-		}
-	});
-	
+			else
+			{
+				console.log(data);	
+				Session.set('trivia_count',data);
+			}
+		}); }, 60000);
+		// This code only runs on the client
+		Template.body.helpers({
+			tasks: function () {
+				return Tasks.find({});
+			}
+		});
+		
+		Template.trivia.helpers({
+			random_trivia: function () {
+				return Session.get('random_trivia');			
+			},
+			counter: function () {
+				return Session.get('counter');			
+			}
+		});
+		
+		Template.trivia.events({
+			"click .get_trivia": function () {
+				Session.set('counter',Session.get('counter')+1);
+				var x = Meteor.call("getTrivia", function(err, data){
+					if(err){
+						console.log(err);
+					}
+					else
+					{
+						console.log(data);	
+						Session.set('random_trivia',"\""+data+" \"");
+					}
+				});
+				
+				
+			}
+		});
+		
+		Template.body.events({
+			"submit .new-task": function (event) {
+				// This function is called when the new task form is submitted
+				
+				var text = event.target.text.value;
+				
+				Meteor.call("addTask", text);
+				
+				// Clear form
+				event.target.text.value = "";
+				
+				// Prevent default form submit
+				return false;
+			},
+			"change .hide-completed input": function (event) {
+				Session.set("hideCompleted", event.target.checked);
+			}		
+		});
+		
+		// Replace the existing Template.body.helpers
+		Template.body.helpers({
+			tasks: function () {
+				if (Session.get("hideCompleted")) {
+					// If hide completed is checked, filter tasks
+					return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+					} else {
+					// Otherwise, return all of the tasks
+					return Tasks.find({}, {sort: {createdAt: -1}});
+				}
+			},
+			hideCompleted: function () {
+				return Session.get("hideCompleted");
+			},
+			incompleteCount: function () {
+				return Tasks.find({checked: {$ne: true}}).count();
+			},
+			trivia_count: function() {
+				return Session.get('trivia_count');
+			}
+		});
+		
+		// In the client code, below everything else
+		Template.task.events({
+			"click .toggle-checked": function () {
+				// Set the checked property to the opposite of its current value
+				Meteor.call("setChecked", this._id, ! this.checked);
+			},
+			"click .delete": function () {
+				Meteor.call("deleteTask", this._id);
+			},
+			"click .toggle-private": function () {
+				Meteor.call("setPrivate", this._id, ! this.private);
+			}
+		});
+		Accounts.ui.config({
+			passwordSignupFields: "USERNAME_ONLY"
+		});
+		Template.task.helpers({
+			isOwner: function () {
+				return this.owner === Meteor.userId();
+			}
+		});
+		
 }
 
 Meteor.methods({
@@ -184,16 +186,32 @@ Meteor.methods({
 	getTriviaCount: function(){
 		this.unblock();
 		try {
-		var maxresult = HTTP.call("GET", "http://45.55.76.36:3555/sevabot/counters?query=%7B%22chat_id%22%3A%22%23j0kur33z33%2F%24612a20053604dfe4%22%7D"
+			var maxresult = HTTP.call("GET", "http://45.55.76.36:3555/sevabot/counters?query=%7B%22chat_id%22%3A%22%23j0kur33z33%2F%24612a20053604dfe4%22%7D"
 			,{params: {limit: 1}}
 			);
 			var max = maxresult.data[0].sequence;
 			return max;
-		} catch (e) {
+			} catch (e) {
+			return false;
+		};
+	},
+	getCounters: function(){
+		this.unblock();
+		try {
+			var sourceCounters = HTTP.call("GET", "http://45.55.76.36:3555/sevabot/counters",false,function(err,result){
+				if(err)
+				console.log(err);
+				else
+				return result.data;
+			});
+			return sourceCounters;
+			} catch (e) {
 			return false;
 		};
 	}
 });
+
+
 
 if (Meteor.isServer) {
 	Meteor.publish("tasks", function () {
@@ -204,4 +222,40 @@ if (Meteor.isServer) {
 			]
 		});
 	});
+	/*
+		getCounterData = function(callback){
+		HTTP.call("GET", "http://45.55.76.36:3555/sevabot/counters",false,
+		function(err, res) {
+		if (err) throw new Error(err.message);
+		else callback && callback( null, res);
+		}
+		);
+		};
+	*/
+	/*
+	setInterval(function(){ 
+		console.log("interval executed");
+		var getCounterDataSynchronously = Meteor.wrapAsync(Meteor.call("getCounters",function(err,sourceCounters){
+			if(err) throw new Error(err.message);
+			else{
+				var sourceCounters = getCounterDataSynchronously().data;
+				for(var i = 0; i < sourceCounters.length;i++){
+					var sourceCounter = sourceCounters[i];
+					//if(Counters.find({"_id":sourceCounter._id}).count()>0)
+					var sourceCount = Counters.find({"_id":sourceCounter._id}).count();
+					console.log("sourceCounter: "+sourceCounter._id);
+				}
+			}
+		}
+		));
+	}, 5000);
+	*/
+	Meteor.setInterval(function(){ 
+			var sourceCounters = HTTP.call("GET", "http://45.55.76.36:3555/sevabot/counters");
+			for(var i = 0; i < sourceCounters.length;i++){
+					var sourceCounter = sourceCounters[i];
+					var sourceCount = Counters.find({"_id":sourceCounter._id}).count();
+					console.log("sourceCounter: "+sourceCounter._id);
+				}
+		},5000);
 }
